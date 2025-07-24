92f2280c88a54024fe4ab4398b53e961414e92bd
Harsh Kumar Kaithwas <stelioswork7@gmail.com>
2025-07-24 21:53:50 +0530

    Update ExpiryReportServlet.java

--- src/main/java/com/servlet/ExpiryReportServlet.java
+++ src/main/java/com/servlet/ExpiryReportServlet.java
@@ -12,23 +12,16 @@ import javax.servlet.http.HttpServletResponse;
 import com.DAO.MedicineDAO;
 import com.entity.Medicine;
 
-// This servlet is mapped to a new URL
 @WebServlet("/ExpiryReportServlet")
 public class ExpiryReportServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // 1. Create an instance of the DAO
+       
         MedicineDAO dao = new MedicineDAO();
-        
-        // 2. Call the new method to get only the expiring medicines
         List<Medicine> expiringList = dao.getExpiringMedicines();
-        
-        // 3. Set the list as an attribute to be used by the JSP
         request.setAttribute("expiringList", expiringList);
-        
-        // 4. Forward the request to a new JSP page that will display the report
         request.getRequestDispatcher("expiryReport.jsp").forward(request, response);
     }
 }
